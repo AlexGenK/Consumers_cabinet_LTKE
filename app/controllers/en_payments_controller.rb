99d6a394d22fcdb41080134f12ec270e4bd9f76f
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-06 22:22:13 +0200

    Implemented index of energy payments

--- app/controllers/en_payments_controller.rb
+++ app/controllers/en_payments_controller.rb
@@ -1,6 +1,14 @@
 class EnPaymentsController < ApplicationController
+  before_action :set_consumer
 
-	def index
-	end
+  def index
+    @en_payments = @consumer.en_payments.all.order(:day)
+  end
+
+  private
+
+  def set_consumer
+    @consumer = Consumer.find(params[:consumer_id])
+  end
 	
 end
