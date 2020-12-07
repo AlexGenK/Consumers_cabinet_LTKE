d5d47772f6dc4654810d663782374bd06413e2f0
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-07 23:35:20 +0200

    Implemented creating a new message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,9 +6,19 @@ class MessagesController < ApplicationController
     @message = @consumer.messages.new
   end
 
+  def create
+    @message = @consumer.messages.new(message_params)
+    flash[:alert] = 'Неможливо створити платіж' unless @message.save
+    redirect_to consumer_messages_path(@consumer)
+  end
+
   private
 
   def set_consumer
     @consumer = Consumer.find(params[:consumer_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:text, :comments, :state)
   end
 end
