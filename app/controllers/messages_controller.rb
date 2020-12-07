28f4c8421bd9db9701f7677972f31662584d5fdf
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-07 20:30:38 +0200

    Added Messages controller

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,12 @@
+class MessagesController < ApplicationController
+  before_action :set_consumer
+
+  def index
+  end
+
+  private
+
+  def set_consumer
+    @consumer = Consumer.find(params[:consumer_id])
+  end
+end
