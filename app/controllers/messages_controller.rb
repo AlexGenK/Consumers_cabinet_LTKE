058a647252e4d17ef2e9ac75be2e9351f9fa9e71
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-07 21:43:06 +0200

    Implemented index of messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   before_action :set_consumer
 
   def index
+    @messages = @consumer.messages.all.order(:created_at)
+    @message = @consumer.messages.new
   end
 
   private
--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,3 +1,4 @@
 class Consumer < ApplicationRecord
 	has_many :en_payments
+	has_many :messages
 end
