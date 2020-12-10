b30ee2393d81c9ab65e3d1e0eebcbaf57a600598
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-10 22:32:18 +0200

    Energy bid can be edited

--- app/controllers/en_bids_controller.rb
+++ app/controllers/en_bids_controller.rb
@@ -1,18 +1,45 @@
 class EnBidsController < ApplicationController
-	before_action :set_consumer
-	before_action :set_en_bid
+  before_action :set_consumer
+  before_action :set_en_bid
 
-	def show
-		@en_bid ||= @consumer.build_en_bid
-	end
+  def show
+  end
 
-	private
+  def edit
+  end
 
-	def set_consumer
+  def update
+    if @en_bid.update(en_bid_params)
+      redirect_to consumer_en_bid_path(@consumer)
+    else
+      flash[:alert] = 'Неможливо відредагувати заявку'
+      render :edit
+    end
+  end
+
+  private
+
+  def set_consumer
     @consumer = Consumer.find(params[:consumer_id])
   end
 
   def set_en_bid
-    @en_bin = Consumer.find(params[:consumer_id]).en_bid
+    @en_bid = Consumer.find(params[:consumer_id]).en_bid
+    @en_bid ||= @consumer.create_en_bid
+  end
+
+  def en_bid_params
+    params.require(:en_bid).permit(:jan_a_1, :jan_b_1, :jan_a_2, :jan_b_2,
+                                   :feb_a_1, :feb_b_1, :feb_a_2, :feb_b_2,
+                                   :mar_a_1, :mar_b_1, :mar_a_2, :mar_b_2,
+                                   :apr_a_1, :apr_b_1, :apr_a_2, :apr_b_2,
+                                   :may_a_1, :may_b_1, :may_a_2, :may_b_2,
+                                   :jun_a_1, :jun_b_1, :jun_a_2, :jun_b_2,
+                                   :jul_a_1, :jul_b_1, :jul_a_2, :jul_b_2,
+                                   :aug_a_1, :aug_b_1, :aug_a_2, :aug_b_2,
+                                   :sep_a_1, :sep_b_1, :sep_a_2, :sep_b_2,
+                                   :okt_a_1, :okt_b_1, :okt_a_2, :okt_b_2,
+                                   :nov_a_1, :nov_b_1, :nov_a_2, :nov_b_2,
+                                   :dec_a_1, :dec_b_1, :dec_a_2, :dec_b_2)
   end
 end
