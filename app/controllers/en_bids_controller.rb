64249df0d70a037ef34e31fd838d306fa9fdfdc8
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-10 21:04:26 +0200

    Energy bid can be shown

--- app/controllers/en_bids_controller.rb
+++ app/controllers/en_bids_controller.rb
@@ -1,11 +1,18 @@
 class EnBidsController < ApplicationController
 	before_action :set_consumer
+	before_action :set_en_bid
+
 	def show
+		@en_bid ||= @consumer.build_en_bid
 	end
 
 	private
 
-	 def set_consumer
+	def set_consumer
     @consumer = Consumer.find(params[:consumer_id])
+  end
+
+  def set_en_bid
+    @en_bin = Consumer.find(params[:consumer_id]).en_bid
   end
 end
