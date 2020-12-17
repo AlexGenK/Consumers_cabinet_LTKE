b5e000b2d82abba3ef1d112a3a2eeb506e6ee602
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-17 16:23:45 +0200

    Realise mock for generating electricity invoice in pdf format

--- app/controllers/en_invoices_controller.rb
+++ app/controllers/en_invoices_controller.rb
@@ -1,12 +1,29 @@
 class EnInvoicesController < ApplicationController
-	before_action :set_consumer
-	
-	def show
-	end
+  before_action :set_consumer
+  
+  def show
+    respond_to do |format|
+      format.html { render :show }
+      format.pdf do
+        send_data pdf_form.render,
+                  filename: "Счет для #{@consumer.name} от #{DateTime.now.strftime('%Y_%m')}.pdf",
+                  type: 'application/pdf',
+                  disposition: 'inline'
+      end
+    end
+  end
 
-	private
+  private
 
-	def set_consumer
+  def pdf_form
+    pdf = Prawn::Document.new
+    pdf.font Rails.root.join("app/assets/fonts/SegoeUI.ttf")
+    pdf.text "Рахунок-фактура споживача електроенергії #{@consumer.name}"
+    pdf.text DateTime.now.strftime('Сгенеровано %F в %T')
+    return pdf
+  end
+
+  def set_consumer
     @consumer = Consumer.find(params[:consumer_id])
   end
 end
