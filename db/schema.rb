a4ae5e2d15519e02fce4a9c8977f3c131c5c77b4
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-22 23:13:02 +0200

    Add unuqiue name index to users model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_21_193445) do
+ActiveRecord::Schema.define(version: 2020_12_22_210350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -225,6 +225,7 @@ ActiveRecord::Schema.define(version: 2020_12_21_193445) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
