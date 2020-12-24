82b03c5c884443be14798d7bad3dc45ba8074328
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-24 22:34:52 +0200

    Added depend destroy to all objects associated with Consumers

--- app/models/consumer.rb
+++ app/models/consumer.rb
@@ -1,15 +1,15 @@
 class Consumer < ApplicationRecord
-	has_many :en_payments
-	has_one  :en_bid
-	has_many :previous_en_consumption
-	has_one  :current_en_consumption
+	has_many :en_payments, dependent: :destroy
+	has_one  :en_bid, dependent: :destroy
+	has_many :previous_en_consumption, dependent: :destroy
+	has_one  :current_en_consumption, dependent: :destroy
 
-	has_many :gas_payments
-	has_one  :gas_bid
-	has_many :previous_gas_consumption
-	has_one  :current_gas_consumption
+	has_many :gas_payments, dependent: :destroy
+	has_one  :gas_bid, dependent: :destroy
+	has_many :previous_gas_consumption, dependent: :destroy
+	has_one  :current_gas_consumption, dependent: :destroy
 
-	has_many :messages
+	has_many :messages, dependent: :destroy
 
 	validates :name, :onec_id, presence: true, uniqueness: true
 
