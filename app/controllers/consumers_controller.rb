2785086a26731a791ef09c718c116ca41c560571
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-26 22:35:12 +0200

    Ability control for Consumers

--- app/controllers/consumers_controller.rb
+++ app/controllers/consumers_controller.rb
@@ -1,14 +1,26 @@
 class ConsumersController < ApplicationController
   before_action :set_consumer, only: [:edit, :update, :destroy]
   before_action :set_users_list, only: [:new, :edit, :create]
+  before_action :detect_invalid_user, only: [:show, :edit, :update, :destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :denied_action
   load_and_authorize_resource
 
 	def index
-		@consumers=Consumer.order(:name)
+		if current_user.admin_role?
+      @consumers=Consumer.order(:name)
+    elsif current_user.manager_role?
+      @consumers=Consumer.where('manager_en_username = ?', current_user.name).
+                or(Consumer.where('manager_gas_username = ?', current_user.name)).
+                order(:name)
+    elsif current_user.client_role?
+      @consumers=Consumer.where('client_username = ?', current_user.name).order(:name)
+    end
 	end
 
 	def new
     @consumer = Consumer.new
+    @consumer.manager_en_username = current_user.name
+    @consumer.manager_gas_username = current_user.name
 	end
 
   def create
@@ -57,5 +69,20 @@ class ConsumersController < ApplicationController
   def set_users_list
     @managers = User.where("manager_role").order(:name).collect(&:name)
     @clients = User.where("client_role").order(:name).collect(&:name)
+  end
+
+  def detect_invalid_user
+    unless current_user.admin_role?
+      if current_user.manager_role?
+        denied_action if (@consumer.manager_en_username != current_user.name) &&
+                         (@consumer.manager_gas_username != current_user.name)
+      elsif current_user.client_role?
+        denied_action if @consumer.client_username != current_user.name
+      end
+    end
+  end
+
+  def denied_action
+    redirect_to :consumers, alert: "Спроба отримати доступ до споживача, що Вам не належить або не існує"
   end
 end
