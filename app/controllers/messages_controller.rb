6b10cd754be7b2ea01afb98ebe6bcc2c48e3baed
Oleksii Kozhhyn <suburbakskin@gmail.com>
2020-12-28 23:19:59 +0200

    Ability control for messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
 class MessagesController < ApplicationController
   before_action :set_consumer
   before_action :set_message, only: [:destroy, :edit, :update]
+  before_action :detect_invalid_user
+  rescue_from ActiveRecord::RecordNotFound, with: :denied_action
   load_and_authorize_resource
 
   def index
@@ -48,5 +50,20 @@ class MessagesController < ApplicationController
 
   def message_params
     params.require(:message).permit(:text, :comment, :state)
+  end
+
+  def detect_invalid_user
+    unless current_user.admin_role?
+      if current_user.manager_role?
+        denied_action if (@consumer.manager_en_username != current_user.name) &&
+                         (@consumer.manager_gas_username != current_user.name)
+      elsif current_user.client_role?
+        denied_action if @consumer.client_username != current_user.name
+      end
+    end
+  end
+
+  def denied_action
+    redirect_to :consumers, alert: "Спроба отримати доступ до запітив споживача, що Вам не належить або не існує"
   end
 end
