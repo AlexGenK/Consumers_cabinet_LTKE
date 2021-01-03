4ca856169e7de0926fae9d5a06547724af67d02c
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-01-03 12:20:47 +0200

    Removed extra functionality from FillingPreviousEnConsumptionsHelper

--- app/helpers/admin/filling_previous_en_consumptions_helper.rb
+++ app/helpers/admin/filling_previous_en_consumptions_helper.rb
@@ -1,18 +1,3 @@
 module Admin::FillingPreviousEnConsumptionsHelper
-	def to_1cid(item)
-	  id = item.to_i
-	  id == 0 ? 10000 + item[3..-1].to_i : id
-	end
-
-	def to_edrpou(item)
-	  item.rjust(8, '0') 
-	end
-
-	def to_dog_en_num(item)
-	  item =~ /Договір №(.*) від/ ? Regexp.last_match[1].strip : nil
-	end
-
-	def to_dog_en_date(item)
-	  item == nil ? nil : Date.strptime(item, '%Y.%m.%d')
-	end
+	include Admin::CsvHelper
 end
