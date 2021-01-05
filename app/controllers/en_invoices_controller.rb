aae6574056934dd0d4d01b6e40e1347c749830d0
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-01-05 23:17:41 +0200

    Calculate energy invoice

--- app/controllers/en_invoices_controller.rb
+++ app/controllers/en_invoices_controller.rb
@@ -6,7 +6,7 @@ class EnInvoicesController < Invoice
     respond_to do |format|
       format.html { render :show }
       format.pdf do
-        data = InvData.new(0, 0, 0, 0, 0)
+        data = calculate_invoice
         send_data pdf_form(data, style: 'energy').render,
                   filename: "Рахунок для #{@consumer.name} від #{DateTime.now.strftime('%Y_%m')}.pdf",
                   type: 'application/pdf',
@@ -16,6 +16,15 @@ class EnInvoicesController < Invoice
   end
 
   private
+
+  def calculate_invoice
+    sum_vat = params[:sum].to_f.round(2)
+    price = params[:tariff].to_f
+    sum = (sum_vat - (sum_vat / 6)).round(2)
+    vat = (sum_vat - sum).round(2)
+    quantity = (sum/price).round(5)
+    InvData.new(quantity, price, sum, vat, sum_vat)
+  end
 
   def set_consumer
     @consumer = Consumer.find(params[:consumer_id])
