eb4ef62c0bd24fa9c8c2776b2050b412fc6bb613
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-01-21 06:27:27 +0200

    Added validations for adjustments

--- app/models/en_adjustment.rb
+++ app/models/en_adjustment.rb
@@ -1,3 +1,4 @@
 class EnAdjustment < ApplicationRecord
   belongs_to :consumer
+  validates :value, numericality: { greater_than: 0 }, presence: true
 end
--- app/models/gas_adjustment.rb
+++ app/models/gas_adjustment.rb
@@ -1,3 +1,4 @@
 class GasAdjustment < ApplicationRecord
   belongs_to :consumer
+  validates :value, numericality: { greater_than: 0 }, presence: true
 end
