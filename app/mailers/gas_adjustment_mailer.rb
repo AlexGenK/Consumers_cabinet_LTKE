073f43abe195757b0955a5365b4634e73b3dbf46
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-01-22 20:31:44 +0200

    Generated sceleton for adjustments mailers

---
+++ app/mailers/gas_adjustment_mailer.rb
@@ -0,0 +1,2 @@
+class GasAdjustmentMailer < ApplicationMailer
+end
---
+++ spec/mailers/previews/gas_adjustment_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/gas_adjustment_mailer
+class GasAdjustmentMailerPreview < ActionMailer::Preview
+
+end
