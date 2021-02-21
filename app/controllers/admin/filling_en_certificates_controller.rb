52468050d1b484fff9c3f9cd4406feb08639cec9
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-02-21 12:12:39 +0200

    Filling certificates with new structure

--- app/controllers/admin/filling_en_certificates_controller.rb
+++ app/controllers/admin/filling_en_certificates_controller.rb
@@ -6,49 +6,37 @@ class Admin::FillingEnCertificatesController < ApplicationController
   require 'net/ftp'
 
   def start
-  	@add_certs = []
-  	@cert_list = get_newest_certs
-  	@cert_list.each do |key, value|
-  		consumer = Consumer.find_by(onec_id: key)
-  		if consumer
-  			@cert = consumer.build_en_certificate(date: value[:date])
-  			@cert.save!
-        Net::FTP.open(ENV['CONSUMERS_CABINET_LTKE_FTP_HOST'], 
-                  ENV['CONSUMERS_CABINET_LTKE_FTP_USERNAME'],
-                  ENV['CONSUMERS_CABINET_LTKE_FTP_PASSWORD'],) do |ftp| 
-        	ftp.chdir('energy/certificates')
-         	ftp.getbinaryfile(value[:filename], 'public/akt.xlsx')
+    @add_certs = []
+    Net::FTP.open(ENV['CONSUMERS_CABINET_LTKE_FTP_HOST'], 
+                  port: ENV['CONSUMERS_CABINET_LTKE_FTP_PORT'],
+                  username: ENV['CONSUMERS_CABINET_LTKE_FTP_USERNAME'],
+                  password: ENV['CONSUMERS_CABINET_LTKE_FTP_PASSWORD'],) do |ftp|
+      ftp.chdir('certificates/en') 
+      ftp.nlst.each do |filename|
+        consumer_id = parse_id(filename)
+        consumer_dog = parse_dog(filename)
+        @consumer = Consumer.find_by(onec_id: consumer_id, dog_num: consumer_dog, energy_consumer: true)
+        if @consumer
+          @consumer.en_certificate&.destroy
+          @cert = @consumer.build_en_certificate
+          @cert.save!
+          ftp.getbinaryfile(filename, 'public/akt.xlsx')
+          @cert.print_form.attach(io: File.open('public/akt.xlsx'), filename: 'akt.xlsx', content_type: 'application/vnd.ms-excel')
+          @add_certs << "Споживач #{@consumer.name} (#{@consumer.edrpou}) - договір №#{@consumer.dog_num}"
         end
-        @cert.print_form.attach(io: File.open('public/akt.xlsx'), filename: 'akt.xlsx', content_type: 'application/vnd.ms-excel')
-        @add_certs << "Потребитель #{consumer.name} (#{consumer.edrpou}) - акт от #{value[:date].strftime("%d.%m.%Y")}"
-  		end
-  	end
+      end
+    end
   end
 
   private
 
-  def get_newest_certs
-    files = {}
-    Net::FTP.open(ENV['CONSUMERS_CABINET_LTKE_FTP_HOST'], 
-                  ENV['CONSUMERS_CABINET_LTKE_FTP_USERNAME'],
-                  ENV['CONSUMERS_CABINET_LTKE_FTP_PASSWORD'],) do |ftp|
-      ftp.chdir('energy/certificates') 
-      ftp.nlst.each do |filename|
-        if !files[parse_id(filename)] || files[parse_id(filename)][:date] < parse_date(filename)
-          files[parse_id(filename)] = {date: parse_date(filename), filename: filename}
-        end
-      end
-    end
-    return files
-  end
-
-  def parse_date(filename)
-    filename =~ /(\d\d\.\d\d\.\d\d\d\d)_.*/
-    DateTime.strptime($1, '%d.%m.%Y')
+  def parse_dog(filename)
+    filename =~ /.+_(.+)\..*/
+    $1.gsub(/\$/, '/')
   end
 
   def parse_id(filename)
-    filename =~ /\d\d\.\d\d\.\d\d\d\d_(.+)/
+    filename =~ /(.+)_.+/
     to_1cid($1)
   end
 end
