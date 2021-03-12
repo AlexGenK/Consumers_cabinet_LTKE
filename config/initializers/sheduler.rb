d8efda5edbeeb2d35710df21d27e7809fbb4b5a0
Oleksii Kozhyn <suburbakskin@gmail.com>
2021-03-12 10:22:21 +0200

    Fix automatic filling consumptions

--- config/initializers/sheduler.rb
+++ config/initializers/sheduler.rb
@@ -14,21 +14,23 @@ s = Rufus::Scheduler.singleton
 def filling_gas_from_file(csv_file)
   csv = CSV.parse(csv_file, col_sep: ';')
     csv.each do |record|
-      @consumer = Consumer.find_by(onec_id: to_1cid(record[0]), dog_num: record[10])
+      @consumer = Consumer.find_by(onec_id: to_1cid(record[0]), dog_num: record[11])
       if @consumer
         @consumer.current_gas_consumption&.destroy
 
         opening_balance = -1*to_money(record[5])
         volume = (@consumer.gas_bid ? @consumer.gas_bid.month_sum(Time.now.month) : 0)
         tariff = to_tariff(record[6])
+        next_tariff = to_tariff(record[7])
         cost = (volume * tariff).round(2)
         cost_val = (cost * 1.2).round(2)
-        money = to_money(record[7])
+        money = to_money(record[8])
         closing_balance = opening_balance - cost_val + money
 
         @consumer.create_current_gas_consumption(opening_balance: opening_balance,
                                                  volume:          volume,
                                                  tariff:          tariff,
+                                                 next_tariff:     next_tariff,
                                                  cost:            cost,
                                                  cost_val:        cost_val,
                                                  money:           money,
@@ -41,21 +43,23 @@ def filling_en_from_file(csv_file)
   @imported = []
   csv = CSV.parse(csv_file, col_sep: ';')
     csv.each do |record|
-      @consumer = Consumer.find_by(onec_id: to_1cid(record[0]), dog_num: record[10])
+      @consumer = Consumer.find_by(onec_id: to_1cid(record[0]), dog_num: record[11])
       if @consumer
         @consumer.current_en_consumption&.destroy
 
         opening_balance = -1*to_money(record[5])
         power = (@consumer.en_bid ? @consumer.en_bid.month_sum(Time.now.month) : 0)
         tariff = to_tariff(record[6])
+        next_tariff = to_tariff(record[7])
         cost = (power * tariff).round(2)
         cost_val = (cost * 1.2).round(2)
-        money = to_money(record[7])
+        money = to_money(record[8])
         closing_balance = opening_balance - cost_val + money
 
         @consumer.create_current_en_consumption(opening_balance: opening_balance,
                                              		power:           power,
                                              		tariff:          tariff,
+                                                next_tariff:     next_tariff,
                                              		cost:            cost,
                                              		cost_val:        cost_val,
                                              		money:           money,
