38dc5b0eeb12e0f374e1054ef88d88429e1a9675
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-05-17 22:13:42 +0300

    Create consumer/client/manager report

--- app/controllers/admin/report_clients_controller.rb
+++ app/controllers/admin/report_clients_controller.rb
@@ -3,5 +3,19 @@ class Admin::ReportClientsController < ApplicationController
   authorize_resource :class => false
 
   def show
+    @en_consumers_user = []
+    @gas_consumers_user = []
+
+    Consumer.where(energy_consumer: true).order(:name).each do |consumer|
+      manager = User.find_by(name: consumer.manager_en_username)
+      client = User.find_by(name: consumer.client_username)
+      @en_consumers_user << [consumer.name, consumer.edrpou, consumer.dog_num, consumer.dog_date, client&.name, manager&.name]
+    end
+
+    Consumer.where(gas_consumer: true).order(:name).each do |consumer|
+      manager = User.find_by(name: consumer.manager_gas_username)
+      client = User.find_by(name: consumer.client_username)
+      @gas_consumers_user << [consumer.name, consumer.edrpou, consumer.dog_num, consumer.dog_date, client&.name, manager&.name]
+    end
   end
 end
