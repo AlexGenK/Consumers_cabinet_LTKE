87935fc5404c481aa0fd303c5f9cb00930fb7833
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-05-20 22:28:18 +0300

    Additional clients show in the clients report

--- app/controllers/admin/report_clients_controller.rb
+++ app/controllers/admin/report_clients_controller.rb
@@ -9,13 +9,15 @@ class Admin::ReportClientsController < ApplicationController
     Consumer.where(energy_consumer: true).order(:name).each do |consumer|
       manager = User.find_by(name: consumer.manager_en_username)
       client = User.find_by(name: consumer.client_username)
-      @en_consumers_user << [consumer.name, consumer.edrpou, consumer.dog_num, consumer.dog_date, client&.name, manager&.name]
+      add_clients = consumer.users.reduce ('') {|sum, item| sum + item.name + ', '}
+      @en_consumers_user << [consumer.name, consumer.edrpou, consumer.dog_num, consumer.dog_date, client&.name, manager&.name, add_clients[0..-3]]
     end
 
     Consumer.where(gas_consumer: true).order(:name).each do |consumer|
       manager = User.find_by(name: consumer.manager_gas_username)
       client = User.find_by(name: consumer.client_username)
-      @gas_consumers_user << [consumer.name, consumer.edrpou, consumer.dog_num, consumer.dog_date, client&.name, manager&.name]
+      add_clients = consumer.users.reduce ('') {|sum, item| sum + item.name + ', '}
+      @gas_consumers_user << [consumer.name, consumer.edrpou, consumer.dog_num, consumer.dog_date, client&.name, manager&.name, add_clients[0..-3]]
     end
   end
 end
