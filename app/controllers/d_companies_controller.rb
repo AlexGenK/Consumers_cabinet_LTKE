05bb46d4aa10218c0b8b2f07bc0fd4232a000700
Oleksii Kozhhyn <suburbakskin@gmail.com>
2021-07-03 11:19:51 +0300

    Distribution company can be added

--- app/controllers/d_companies_controller.rb
+++ app/controllers/d_companies_controller.rb
@@ -2,5 +2,19 @@ class DCompaniesController < ApplicationController
   load_and_authorize_resource
 
   def index
+    @d_companies = DCompany.order(:name)
+    @d_company = DCompany.new
+  end
+
+  def create
+    @d_company = DCompany.new(d_company_params)
+    @d_company.save
+    redirect_to d_companies_path
+  end
+
+  private
+
+  def d_company_params
+    params.require(:d_company).permit(:name, :operational)
   end
 end
